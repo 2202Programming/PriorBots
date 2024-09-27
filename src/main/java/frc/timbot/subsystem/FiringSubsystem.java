272ee5d9a5d7219e62c69059ef211fc51a58afe5
DerekL <derek@adminnet.us>
2024-09-26 20:18:55 -0500

    added files for Firing work to be done

---
+++ src/main/java/frc/timbot/subsystem/FiringSubsystem.java
@@ -0,0 +1,9 @@
+package frc.timbot.subsystem;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class FiringSubsystem extends SubsystemBase {
+    //@Pronav, @Jonathan, @Maddy  put the solenoid stuff here
+    
+    
+}
