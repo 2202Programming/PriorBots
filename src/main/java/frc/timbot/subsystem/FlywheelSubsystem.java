91cce86ccba89a1ac35d09be3eb45051b4f1e259
Ben Grogan <26grogab@elmbrookstudents.org>
2024-10-06 19:49:56 -0500

    Started working on flywheel, updated (some) libraries to Phoenix6

--- src/main/java/frc/timbot/subsystem/FlywheelSubsystem.java
+++ src/main/java/frc/timbot/subsystem/FlywheelSubsystem.java
@@ -1,7 +1,27 @@
 package frc.timbot.subsystem;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FlywheelSubsystem extends SubsystemBase {
-    
+    private TalonFX motor1; // motor closer to the solenoid
+    private TalonFX motor2; // motor closer to the output
+
+    public void Flywheel(TalonFX motor1, TalonFX motor2) {
+        this.motor1 = motor1;
+        this.motor2 = motor2;
+    }
+
+    public void speed(double speed) {
+        motor1.set(speed);
+        motor2.set(speed);
+    }
+
+    public boolean isAtSpeed() {
+        if ((motor1.getVelocity() >= 0) && (motor2.getVelocity() >= 0)) {
+            return true;
+        }
+        return false;
+    }
 }
