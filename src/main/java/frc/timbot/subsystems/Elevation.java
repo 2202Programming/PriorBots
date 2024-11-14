b930136bb0b96b139f33897266d9ce63dae16b93
Gavin <gavin.r.longley@gmail.com>
2024-11-13 19:05:28 -0600

    Added some extra voltage reading code
    gonna check if we are even reading voltage

--- src/main/java/frc/timbot/subsystems/Elevation.java
+++ src/main/java/frc/timbot/subsystems/Elevation.java
@@ -7,80 +7,43 @@
 
 package frc.timbot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.timbot.Constants;
-import frc.timbot.utils.Stick;
+
 
 public class Elevation extends SubsystemBase {
   /**
    * Creates a new Elevation.
    */
 
-  public TalonSRX actuator;
+  public CANSparkMax actuator;
   public AnalogInput volts = new AnalogInput(0);
   public PIDController linearControlPID = new PIDController(getPosition(), getPosition(), getPosition());
 
-  public Elevation(TalonSRX actuator) {
-    this.actuator = actuator;
-
-    // https://github.dev/CrossTheRoadElec/Phoenix-Examples-Languages
-
-    // Initialization procedure brought from
-    // https://github.com/CrossTheRoadElec/Phoenix-Examples-Languages/blob/master/Java%20Talon%20FX%20(Falcon%20500)/PositionClosedLoop/src/main/java/frc/timbot/timbot.java
-    this.actuator.configFactoryDefault();
-    this.actuator.configSelectedFeedbackSensor(FeedbackDevice.PulseWidthEncodedPosition,
-        Constants.Elevation.kPIDLoopIdx, Constants.Elevation.kTimeoutMs);
-
-    this.actuator.setSensorPhase(Constants.Elevation.kSensorPhase);
-    this.actuator.setInverted(Constants.Elevation.kMotorInvert);
-
-    this.actuator.configNominalOutputForward(0, Constants.Elevation.kTimeoutMs);
-    this.actuator.configNominalOutputReverse(0, Constants.Elevation.kTimeoutMs);
-    this.actuator.configPeakOutputForward(1, Constants.Elevation.kTimeoutMs);
-    this.actuator.configPeakOutputReverse(-1, Constants.Elevation.kTimeoutMs);
-
-    /**
-     * Config the allowable closed-loop error, Closed-Loop output will be neutral
-     * within this range. See Table in Section 17.2.1 for native units per rotation.
-     */
-    this.actuator.configAllowableClosedloopError(0, Constants.Elevation.kPIDLoopIdx, Constants.Elevation.kTimeoutMs);
-
-    /* Config Position Closed Loop gains in slot0, tsypically kF stays zero. */
-    this.actuator.config_kF(Constants.Elevation.kPIDLoopIdx, Constants.Elevation.kGains.kF,
-        Constants.Elevation.kTimeoutMs);
-    this.actuator.config_kP(Constants.Elevation.kPIDLoopIdx, Constants.Elevation.kGains.kP,
-        Constants.Elevation.kTimeoutMs);
-    this.actuator.config_kI(Constants.Elevation.kPIDLoopIdx, Constants.Elevation.kGains.kI,
-        Constants.Elevation.kTimeoutMs);
-    this.actuator.config_kD(Constants.Elevation.kPIDLoopIdx, Constants.Elevation.kGains.kD,
-        Constants.Elevation.kTimeoutMs);
-
-    setActuatorAngle(0);
+  public Elevation(CANSparkMax actuator) {
+    volts.setAverageBits(4);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    System.out.println(this.getPosition());
   }
 
   public void setActuatorAngle(double angle) {
-    actuator.set(ControlMode.Position, angleToPosition(angle));
-    Stick.log("Running setActuatorAngle angle= " + angle);
+
   }
 
-  public double angleToPosition(double angle) {
+  public double getHeight() {
     double height = 17.72 + this.getPosition();
     return height;
   }
 
-  public double getPosition() { //in centimeters
-    return volts.getVoltage() * 1.2 * 2.54; //basiclly 0-5 volts * 6 inches/5 * conversion inches to cm
+  public double getPosition() { //in inches
+    System.out.println(volts.getVoltage());
+    return volts.getVoltage() * 1.2; //basiclly 0-5 volts * 6 inches/5
   }
 
 }
