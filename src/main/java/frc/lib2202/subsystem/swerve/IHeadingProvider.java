405838148f26a5ce7fc1a03f7e43f6dee058d639
DerekL <derek@adminnet.us>
2025-10-02 18:02:04 -0500

    removed static copy of lib2202

--- src/main/java/frc/lib2202/subsystem/swerve/IHeadingProvider.java
+++
@@ -1,9 +0,0 @@
-package frc.lib2202.subsystem.swerve;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-
-public interface IHeadingProvider {
-    public Rotation2d getRotation2d();
-
-    default public Rotation2d getHeading() { return getRotation2d(); }
-}
